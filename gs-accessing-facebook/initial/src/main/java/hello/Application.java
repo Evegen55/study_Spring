c9a360a9b33ec511c723a18e0dabb957ab1ddbbb
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-12-12 18:27:06 +0300

    add demo from https://spring.io/guides/gs/accessing-facebook/

---
+++ gs-accessing-facebook/initial/src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * @author Evgenii_Lartcev (created on 12/12/2016).
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
