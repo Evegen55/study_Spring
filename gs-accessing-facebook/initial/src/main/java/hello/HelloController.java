390ac5978ba8dafa52aba99a89514018c3a08e76
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-12-14 17:44:18 +0300

    Error message is (#12) bio field is deprecated for versions v2.8 and higher

--- gs-accessing-facebook/initial/src/main/java/hello/HelloController.java
+++ gs-accessing-facebook/initial/src/main/java/hello/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.PagedList;
 import org.springframework.social.facebook.api.Post;
+import org.springframework.social.facebook.api.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,8 +30,20 @@ public class HelloController {
         if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
             return "redirect:/connect/facebook";
         }
-
-        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
+        /*
+        an old version rises exception Error message is (#12) bio field is deprecated for versions v2.8 and higher
+        final User userProfile = facebook.userOperations().getUserProfile();
+         */
+        //how solve this problem see http://stackoverflow.com/questions/39890885/error-message-is-12-bio-field-is-deprecated-for-versions-v2-8-and-higher
+        //{ "id", "about", "age_range", "birthday", "context", "cover", "currency", "devices", "education", "email",
+        // "favorite_athletes", "favorite_teams", "first_name", "gender", "hometown", "inspirational_people", "installed",
+        // "install_type", "is_verified", "languages", "last_name", "link", "locale", "location", "meeting_for",
+        // "middle_name", "name", "name_format", "political", "quotes", "payment_pricepoints", "relationship_status",
+        // "religion", "security_settings", "significant_other", "sports", "test_group", "timezone", "third_party_id",
+        // "updated_time", "verified", "video_upload_limits", "viewer_can_send_gift", "website", "work"}
+        String [] fields = { "id", "email",  "first_name", "last_name" };
+        User userProfile = facebook.fetchObject("me", User.class, fields);
+        model.addAttribute("facebookProfile", userProfile);
         PagedList<Post> feed = facebook.feedOperations().getFeed();
         model.addAttribute("feed", feed);
         return "hello";
